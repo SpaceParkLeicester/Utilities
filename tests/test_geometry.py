eddcceb51fc2fda1366b16cf43d15a4c17d35a4e
vrym2 <vardhan609@gmail.com>
2023-12-11 17:48:25 +0000

    update

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -19,7 +19,11 @@ def test_raster_data_grid_zones():
 
 def test_grid_zones():
     """Testing WGS84 grid zones"""
+    from pprint import pprint
     bounds = [30.0, -100.0, 34.9, -95.0]
     utm = utm_grid_zones(log=logging)
-    grid_zones = utm.createZoneTable(bounding_box_coords=bounds, intervals=2)
+    intervals = utm.equal_intervals_dividend(upper_limit = bounds[2], lower_limit = bounds[0])
+    grid_zones = utm.createZoneTable(bounding_box_coords=bounds, intervals=intervals)
     assert len(grid_zones) is not None
+    pprint(grid_zones)
+    

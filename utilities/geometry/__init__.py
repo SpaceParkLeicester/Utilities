53c2d8013e24deb6b417bae3e4f4b7bbb56dd0bd
vrym2 <vardhan609@gmail.com>
2023-12-12 14:41:13 +0000

    update

--- utilities/geometry/__init__.py
+++ utilities/geometry/__init__.py
@@ -3,3 +3,4 @@ from .buffer import clip_vector
 from .drivers import save_vector
 from .read import bytes_to_geopandas, geo_pandas, geojson_to_geopandas, web_read_shape_file
 from .utm_zone import utm_grid_zones
+from .clip import raster_clip
--- utilities/geometry/clip.py
+++ utilities/geometry/clip.py
@@ -1,9 +1,89 @@
-"""Function to clip a vector layer with coordinates extent"""
-from logging import config
+"""
+Function to clip a raster and vector layer with coordinates extent, and other ways!
+"""
+"""
+    Clip a Planet raster image
+"""
+import os
+import json
+import numpy as np
+import geojson
+import geopandas as gpd
+from shapely.wkt import loads
+from math import sqrt
+import rasterio as rio
+from rasterio.mask import mask
+from shapely.geometry import shape
+from shapely.geometry import mapping
 
-config.fileConfig("logger.ini")
+class raster_clip:
+    """Function to clip the raster data"""
+    def __init__(
+            self,
+            log: isinstance = None,
+            raster_file:str = None) -> None:
+        r"""Defining variables
+        
+        Args:\n
+            log: custom logger ini file
+            raster_file: Path to the raster file.
+        """
+        self.log = log
+        self.raster_file = raster_file
 
+    def bbox_geom_center(
+        self,
+        center_lat: np.uint64 = None,
+        center_lon: np.uint64 = None,
+        half_side: np.int16 = 10): # Km
+        """Bounding box for a given coordinates pair"""
+        # Sanity check
+        assert half_side > 0
+        assert center_lat >= -90.0 and center_lat  <= 90.0
+        assert center_lon >= -180.0 and center_lon <= 180.0
 
-class gdal_functions:
-    # TODO
-    pass
+        # Km to m
+        half_side = (half_side*1000)/sqrt(2)
+
+        # Geopandas geo-series
+        gs = gpd.GeoSeries(loads(f'POINT({center_lon} {center_lat})'))
+        # GeoDataFrame
+        gdf = gpd.GeoDataFrame(geometry=gs)
+        # Projection
+        gdf.crs='EPSG:4326'
+        gdf = gdf.to_crs('EPSG:3857')
+        res = gdf.buffer(
+            distance=half_side,
+            cap_style=3,
+        )
+
+        # Get the geom
+        gdf = res.to_crs('EPSG:4326')
+        geom = gdf.iloc[0]
+        geojson_string = geojson.dumps(mapping(loads(geom.wkt)))
+        self.geojson_dict = json.loads(geojson_string)
+        return self.geojson_dict
+
+    def clipped(self, clipped_raster: str = None)-> None:
+        """Clipping the raster file"""
+        # Folder to save the file
+        feature_shape = shape(self.geojson_dict)
+        # Clipping the raster
+        with rio.open(self.raster_file) as src:
+            out_image, out_transform = mask(src, [feature_shape], invert = True)
+            out_meta = src.meta.copy()
+
+        # Writing the metadata
+        out_meta.update({"driver": "GTiff",
+            "height": out_image.shape[1],
+            "width": out_image.shape[2],
+            "transform": out_transform})
+
+        # Writing the file
+        with rio.open(clipped_raster, "w", **out_meta) as dest:
+            dest.write(out_image)
+
+        try:
+            assert os.path.exists(clipped_raster)
+        except AssertionError:
+            self.log.debug(f"Raster data has not been clipped!")
